c7d15912d01687f426af742a7c1bdfe3030f52a8
Pawel Korus <pawelkorus@ymail.com>
2015-05-26 12:12:41 +0200

    Statistic module: calculating mean

--- tests/statistic.js
+++ tests/statistic.js
@@ -26,6 +26,8 @@ var check = function(expected, op) {
 var data = [
 ['Variance', testData, [0, 1.62, 17701.67453, 12335.0344, 10180.43292, 8903.091467, 8056.196895, 7321.318335], statistic.Variance()],
 ['Variance k=4', testData, [0, 1.62, 17701.67453, 12335.0344, 12424.24773, 10884.88973, 3346.5625, 3771.45509], statistic.Variance(4)],
+['Mean', testData, [3, 3.9, 80.71333333, 69.16, 55.528, 66.77333333, 57.23428571, 50.023125], statistic.Mean()],
+['Mean(4)', testData, [3, 3.9, 80.71333333, 69.16, 68.66, 98.21, 39.625, 30.88625], statistic.Mean(4)]
 ];
 
 data.forEach(function(v) {
